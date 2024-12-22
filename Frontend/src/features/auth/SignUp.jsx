bae5c773dbcec8b1ff1cd2ca943fea0227d4dfd9
Akhilesh26 <akhileshpimple3@gmail.com>
2024-12-22 22:23:24 +0530

    create dir features add auth feater, create signup feature

---
+++ Frontend/src/features/auth/SignUp.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { TextField, Button } from '@mui/material';
+
+const SignUp = () => {
+  return (
+    <div className='h-screen flex justify-center items-center bg-gradient-to-b from-[#3B82F6] to-[#14B8A6]'>
+      <div className='border-2 border-gray-300 p-8 rounded-md bg-white shadow-lg'>
+        <h2 className='text-xl font-bold mb-4 text-center text-blue-500 '>Sign Up</h2>
+        <form className='flex flex-col space-y-4'>
+          {/* Full Name */}
+          <TextField
+            id="full-name"
+            label="Full Name"
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            required
+          />
+
+          {/* Username */}
+          <TextField
+            id="username"
+            label="Username"
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            required
+          />
+
+          {/* Email */}
+          <TextField
+            id="email"
+            label="Email"
+            type="email"
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            required
+          />
+
+          {/* Password */}
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            required
+          />
+
+          {/* Submit Button */}
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            
+          >
+            Sign Up
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
