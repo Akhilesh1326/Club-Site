998b4ffd5e033d10769a7773618f780fdb15cdb4
Akhilesh26 <akhileshpimple3@gmail.com>
2024-12-24 15:30:21 +0530

    add theme provider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./Theme";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import LandingPage from "./pages/LandingPage"
+import SignUp from './features/auth/SignUp.jsx';
+const router = createBrowserRouter([
+  { path: "/", element: <LandingPage /> },
+  { path: "/sign-up", element: <SignUp/>}
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <div>
+        <RouterProvider router={router} />
+      </div>
+    </ThemeProvider>
   </StrictMode>,
 )
