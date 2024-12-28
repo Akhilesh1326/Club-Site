f8cc60352ed146eec78c4ad4035f6f42c000fe19
Akhilesh26 <akhileshpimple3@gmail.com>
2024-12-28 23:47:42 +0530

    change in routes of landing page

--- Frontend/router.jsx
+++ Frontend/router.jsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 
-// import LandingPage from "./src/pages/LandingPage.jsx";
 import LandingPage from "./src/pages/LandingPage.jsx";
 import SignUp from './src/features/auth/SignUp.jsx';
 import SingUpRole from './src/features/auth/SingUpRole.jsx';
+import SignIn from "./src/features/auth/SignIn.jsx";
 const router = createBrowserRouter([
     { path: "/", element: <LandingPage /> },
     { path: "/sign-up", element: <SignUp/>},
     { path: "/sign-up-role", element: <SingUpRole/>},
+    { path: "/sign-in", element: <SignIn/>},
   ])
 
 export default router;
