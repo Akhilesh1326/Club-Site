bf9ff6d57da6c35a90cad9249ef544d2c008d7ae
Akhilesh26 <akhileshpimple3@gmail.com>
2025-01-04 22:49:16 +0530

    create club leader dashboard and done init

---
+++ Backend/Services/user-management-service/src/main/java/com/example/user_management/user_management_service/Model/Users.java
@@ -0,0 +1,4 @@
+package com.example.user_management.user_management_service.Model;
+
+public class Users {
+}
---
+++ Frontend/src/features/dashboard/ClubLeaderDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ClubLeaderDashboard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ClubLeaderDashboard
