1caa553e26b321db1ce3dedc827215afb90e05ae
Akhilesh26 <akhileshpimple3@gmail.com>
2025-01-06 23:55:24 +0530

    add users schema desgin

--- Backend/Services/user-management-service/src/main/java/com/example/user_management/user_management_service/Model/Users.java
+++ Backend/Services/user-management-service/src/main/java/com/example/user_management/user_management_service/Model/Users.java
@@ -1,4 +1,11 @@
 package com.example.user_management.user_management_service.Model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class Users {
+
 }
