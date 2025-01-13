1eb89d9f11e023b3a7d236575ca4a1754bb5733c
Akhilesh26 <akhileshpimple3@gmail.com>
2025-01-13 21:49:52 +0530

    reate repo and add auth to it

--- Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/Repo/AuthUserRepository.java
+++ Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/Repo/AuthUserRepository.java
@@ -1,4 +1,10 @@
 package com.example.auth.auth_service.Repo;
 
-public class AuthUserRepository {
+
+import com.example.auth.auth_service.Model.Auth_Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+public interface AuthUserRepository extends JpaRepository<Auth_Users, Long>{
+    Optional<Auth_Users> findByEmail(String email);
 }
