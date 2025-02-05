6364011ebd58f36be4662ce6adc546e4957f8df0
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-05 22:10:50 +0530

    add new model for club members

---
+++ Backend/Services/club-management-service/src/main/java/com/example/club_management/club_management_service/Model/ClubMemberModel.java
@@ -0,0 +1,37 @@
+package com.example.club_management.club_management_service.Model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "clubMembers")
+public class ClubMemberModel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private UUID userId;
+    private UUID clubId;
+
+    private Role role;
+    private Hirarchy hirarchy;
+    public enum Role{
+        President,
+        VicePresident,
+        Secretary,
+        Treasurer,
+        Event_Coordinator,
+        Public_Relations_Officer,
+        Marketing_Head,
+        Sponsorship_Coordinator,
+        Technical_Lead,
+        Content_Writer,
+        Design_Head,
+        Webmaster
+         
+        }
+    public enum Hirarchy{
+
+    }
+    
+}
