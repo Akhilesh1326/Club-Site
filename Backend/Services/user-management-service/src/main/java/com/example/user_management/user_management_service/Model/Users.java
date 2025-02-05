e9614047c8bac71d8adbe8cff5ebb5c284dbf8d0
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-05 22:13:44 +0530

    update model for new coloumsn and enum with getter setter methods

--- Backend/Services/user-management-service/src/main/java/com/example/user_management/user_management_service/Model/Users.java
+++ Backend/Services/user-management-service/src/main/java/com/example/user_management/user_management_service/Model/Users.java
@@ -3,52 +3,104 @@ package com.example.user_management.user_management_service.Model;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "users")
 public class Users {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private UUID id;
 
     @Column(nullable = false)
-    private String name;
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+
+    @Column(nullable = false)
+    private LocalDate DOB;
+
+    private String CollegeOrUniversityName;
+
+    private Role role;
+
+    private Number phoneNumber;
 
     @Column(name = "joined_at", nullable = false)
     private LocalDateTime joinedAt;
 
-    @Column(name = "auth_user_id", nullable = false)
-    private Long authUser;
 
     @Column(name = "profile_picture_url", nullable = true)
     private String profilePictureUrl;
+
+
+
 
     @PrePersist
     protected void onJoin(){
         this.joinedAt = LocalDateTime.now();
     }
 
-    public String getName(){
-        return this.name;
-    }
-    public void setName(String name){
-        this.name = name;
+    public enum Role{
+        Fresher,
+        Club_Member,
+        Club_President,
+        General_Participant,
+        Event_Organizer,
     }
 
-    public Long getId(){
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    public Role getRole() {
+        return this.role;
+    }
+
+    public void setCollegeName(String collegeName) {
+        this.CollegeOrUniversityName = collegeName;
+    }
+    public String getCollegeName(){
+        return this.CollegeOrUniversityName;
+    }
+
+    public void setDOB(LocalDate DOB) {
+        this.DOB = DOB;
+    }
+    public LocalDate getDOB() {
+        return this.DOB;
+    }
+
+    public void setPhoneNumber(Number phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public Number getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+
+    public UUID getId(){
         return this.id;
     }
-    public void setId(Long id){
+    public void setId(UUID id){
         this.id = id;
     }
 
-    public Long getAuthUser() {
-        return authUser;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getFirstName() {
+        return this.firstName;
     }
 
-    public void setAuthUser(Long authUser) {
-        this.authUser = authUser;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
     }
 
     public String getProfilePictureUrl(){
