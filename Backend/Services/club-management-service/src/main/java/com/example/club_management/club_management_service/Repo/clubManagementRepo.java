40c12d4926bffc803a3e299371fb474f4438e354
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-05 22:27:29 +0530

    add new method for routes for getting club by user id and club id

--- Backend/Services/club-management-service/src/main/java/com/example/club_management/club_management_service/Repo/clubManagementRepo.java
+++ Backend/Services/club-management-service/src/main/java/com/example/club_management/club_management_service/Repo/clubManagementRepo.java
@@ -6,8 +6,11 @@ import com.example.club_management.club_management_service.Model.ClubModel;
 
 import javax.swing.text.html.Option;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface clubManagementRepo extends JpaRepository<ClubModel, Long> {
     Optional<ClubModel> findByName(String name);
+    ClubModel findClubByClubId(UUID id);
+    ClubModel findClubByUserId(UUID userId);
 }
