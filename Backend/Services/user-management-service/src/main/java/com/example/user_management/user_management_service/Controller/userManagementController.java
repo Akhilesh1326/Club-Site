957795e60e915ed1ca1c008472b55f8887b9ad8f
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-05 22:22:06 +0530

    add new method for getting club by userid and club id

--- Backend/Services/user-management-service/src/main/java/com/example/user_management/user_management_service/Controller/userManagementController.java
+++ Backend/Services/user-management-service/src/main/java/com/example/user_management/user_management_service/Controller/userManagementController.java
@@ -10,23 +10,43 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api")
+@RequestMapping("/api/user-management")
 public class userManagementController {
     @Autowired
     private userManagmentService userManagementService;
 
-    @PostMapping("/user-managment")
-    public ResponseEntity<Map<String, Object>> createUser(@RequestBody UserManagmentDTO userDTO) {
-        Users user = userManagementService.addUser(userDTO);
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("email", user.getName());
-        response.put("auth-user", user.getAuthUser());
-        response.put("profile-picture-url", user.getProfilePictureUrl());
-
+    @GetMapping("/greet")
+    public ResponseEntity<Map<String, String>> greet() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "hello world");
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/create-user/{authId}")
+    public ResponseEntity<Map<String, Object>> createUser(@PathVariable UUID authId, @RequestBody UserManagmentDTO userDTO) {
+        Users user = userManagementService.addUser(userDTO, authId);
+        Map<String, Object> response = new HashMap<>();
+        response.put("First Name", user.getFirstName());
+        response.put("Last Name", user.getLastName());
+        response.put("Date of Birth", user.getDOB());
+        response.put("College name", user.getCollegeName());
+        response.put("Role", user.getRole());
+        response.put("Phone Number", user.getPhoneNumber());
+        response.put("profile-picture-url", user.getProfilePictureUrl());
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/get-user/{userId}")
+    public ResponseEntity<Users> getUser(@PathVariable UUID userId){
+        return ResponseEntity.ok(userManagementService.getAllDataById(userId));
+    }
+
+    @PostMapping("/update-user/{userId}")
+    public ResponseEntity<String> updateUser(@PathVariable UUID userId, @RequestBody UserManagmentDTO userDTO){
+        return ResponseEntity.ok("hello");
     }
 }
