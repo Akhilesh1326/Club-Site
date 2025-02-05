4650d517ee486418e829c906d3607fcd6d1728eb
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-05 22:17:24 +0530

    add feature for checking same emails in db

--- Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/Controller/AuthController.java
+++ Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/Controller/AuthController.java
@@ -2,21 +2,27 @@ package com.example.auth.auth_service.Controller;
 
 import com.example.auth.auth_service.DTO.AuthUserDTO;
 import com.example.auth.auth_service.Model.Auth_Users;
+import com.example.auth.auth_service.Repo.AuthUserRepository;
 import com.example.auth.auth_service.Service.AuthUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api")
+@RequestMapping("/api/auth-service")
 public class AuthController {
 
     @Autowired
     private AuthUserService authUserSerivce;
+    @Autowired
+    private AuthUserRepository authUserRepository;
 
     @GetMapping("/greet")
     public ResponseEntity<Map<String, String>> greet() {
@@ -27,17 +33,29 @@ public class AuthController {
 
     @PostMapping("/register")
     public ResponseEntity<Map<String, Object>> registerAuthUser(@RequestBody AuthUserDTO authUserDTO) {
-//        System.out.println("Received data: " + authUserDTO.getPassword());
+
         Auth_Users registeredUser = authUserSerivce.register(authUserDTO);
 
 
         // Create a response map to send back necessary details
         Map<String, Object> response = new HashMap<>();
+        response.put("id", registeredUser.getUserId());
         response.put("email", registeredUser.getEmail());
         response.put("password", registeredUser.getPassword());
-        response.put("role", registeredUser.getRole());
-
-
         return ResponseEntity.ok(response);
     }
+
+    @GetMapping("/email")
+    public ResponseEntity<String> getEmailById(@RequestParam Long id) {
+        // Call the repository or service method to fetch the email by ID
+        Optional<Auth_Users> user = authUserRepository.findById(id);
+
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get().getEmail());  // Return email if user exists
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+    }
+
+
 }
