5bf97be0710a97b695f57c1829651c428b2600a6
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-05 22:20:12 +0530

    add new method for getting details

--- Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/Repo/AuthUserRepository.java
+++ Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/Repo/AuthUserRepository.java
@@ -3,8 +3,12 @@ package com.example.auth.auth_service.Repo;
 
 import com.example.auth.auth_service.Model.Auth_Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 public interface AuthUserRepository extends JpaRepository<Auth_Users, Long>{
     Optional<Auth_Users> findByEmail(String email);
+    Optional<Auth_Users> findByUserName(String  userName);
+    @Query("SELECT u.email FROM Auth_Users u WHERE u.id = :id")
+    String findEmailById(Long id);
 }
