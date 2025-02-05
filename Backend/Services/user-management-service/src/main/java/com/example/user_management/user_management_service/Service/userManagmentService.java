f412bba8b4a88b4c378e9d35daefd82afb4ebdae
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-05 22:15:30 +0530

    update service for new add coloumns

--- Backend/Services/user-management-service/src/main/java/com/example/user_management/user_management_service/Service/userManagmentService.java
+++ Backend/Services/user-management-service/src/main/java/com/example/user_management/user_management_service/Service/userManagmentService.java
@@ -1,40 +1,49 @@
 package com.example.user_management.user_management_service.Service;
 
 import com.example.user_management.user_management_service.DTO.UserManagmentDTO;
-import com.example.user_management.user_management_service.FeignClient.AuthServiceClient;
 import com.example.user_management.user_management_service.Model.Users;
 import com.example.user_management.user_management_service.Repo.UsersRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class userManagmentService {
     private final UsersRepository usersRepository;
-    private final AuthServiceClient authServiceClient;
+
 
     // Constructor Injection
-    public userManagmentService(UsersRepository usersRepository, AuthServiceClient authServiceClient) {
+    public userManagmentService(UsersRepository usersRepository) {
         this.usersRepository = usersRepository;
-        this.authServiceClient = authServiceClient;
     }
 
-    public Users addUser(UserManagmentDTO userDTO) {
+    public Users addUser(UserManagmentDTO userDTO, UUID authId) {
         // Create a new user
         Users users = new Users();
-        users.setName(userDTO.getName());
 
-        // Fetch auth-user ID from AuthService
-        Map<String, Object> authResponse = authServiceClient.getAuthUser(userDTO.getAuth_user());
-
-        // Assuming the response contains an "auth-user" field with the ID
-        Long authUserId = (Long) authResponse.get("auth-user");
-        users.setAuthUser(authUserId);
-
-        // Set profile picture URL
+        users.setId(authId);
+        users.setFirstName(userDTO.getFirstName());
+        users.setLastName(userDTO.getLastName());
+        users.setDOB(userDTO.getDOB());
+        users.setRole(convertToRole(userDTO.getRole()));
+        users.setCollegeName(users.getCollegeName());
+        users.setPhoneNumber(users.getPhoneNumber());
         users.setProfilePictureUrl(userDTO.getProfile_picture_url());
 
         // Save the user
         return usersRepository.save(users);
+    }
+
+    private Users.Role convertToRole(String role) {
+        try {
+            return Users.Role.valueOf(role);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            throw new IllegalArgumentException("Invalid role" + role);
+        }
+    }
+
+    public Users getAllDataById(UUID userId){
+        return usersRepository.getByUserId(userId);
     }
 }
