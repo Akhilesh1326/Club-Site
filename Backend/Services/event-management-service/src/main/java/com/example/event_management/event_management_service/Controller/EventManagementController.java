f7a0911b0702f327920b84bf4a42147a93a527ac
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-08 22:10:22 +0530

    update route

--- Backend/Services/event-management-service/src/main/java/com/example/event_management/event_management_service/Controller/EventManagementController.java
+++ Backend/Services/event-management-service/src/main/java/com/example/event_management/event_management_service/Controller/EventManagementController.java
@@ -5,14 +5,20 @@ package com.example.event_management.event_management_service.Controller;
 import com.example.event_management.event_management_service.DTO.EventManagementDTO;
 import com.example.event_management.event_management_service.Model.EventManagementModel;
 import com.example.event_management.event_management_service.Service.EventManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin
 @RestController
-@RequestMapping("/api/event-management")
+@RequestMapping("/api/eventManagement")
 public class EventManagementController {
     private final EventManagementService eventManagementService;
+    @Autowired
+    private EventManagementModel eventManagementModel;
+
     public EventManagementController(EventManagementService eventManagementService){
         this.eventManagementService = eventManagementService;
     }
@@ -20,6 +26,17 @@ public class EventManagementController {
     @PostMapping("/create-event")
     public ResponseEntity<?> createEvent(@RequestBody EventManagementDTO eventManagementDTO){
         return ResponseEntity.ok(eventManagementService.createEvent(eventManagementDTO));
+    }
+
+    @GetMapping("event")
+    public ResponseEntity<List<EventManagementModel>> getAllEvents(){
+        List<EventManagementModel> events = eventManagementService.getAllEvents();
+    }
+
+
+    @GetMapping("/get-event-by-category/{category}")
+    public List<EventManagementModel> getEventByCategory(@RequestParam String category){
+        return ResponseEntity.ok(eventManagementService.getEventByCategory(category));
     }
 
 }
