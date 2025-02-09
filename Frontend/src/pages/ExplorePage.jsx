9a724fd2bf2becb35268490c7ebb25cb6ed69054
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-09 20:26:27 +0530

    add breadcrumbs

--- Frontend/src/pages/ExplorePage.jsx
+++ Frontend/src/pages/ExplorePage.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CardActionArea from '@mui/material/CardActionArea';
+
+
+function handleClick(event) {
+  event.preventDefault();
+  console.info('You clicked a breadcrumb.');
+}
 
 const ExplorePage = () => {
+
   return (
     <div>
-      
+      <div role="presentation" onClick={handleClick}>
+        <Breadcrumbs aria-label="breadcrumb">
+          <Link underline="hover" color="inherit" href="/">
+            MUI
+          </Link>
+          <Link
+            underline="hover"
+            color="inherit"
+            href="http://localhost:5173/home"
+          >
+            Home
+          </Link>
+          <Typography sx={{ color: 'text.primary' }}>Breadcrumbs</Typography>
+        </Breadcrumbs>
+
+      </div>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              Lizards are a widespread group of squamate reptiles, with over 6,000
+              species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </div>
   )
 }
