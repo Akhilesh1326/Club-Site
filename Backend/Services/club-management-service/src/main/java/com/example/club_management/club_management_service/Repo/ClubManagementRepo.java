cc0bf0fbd6edb04b1bb29854c741b78e9826988c
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-15 04:17:57 +0530

    add method for retriving info

--- Backend/Services/club-management-service/src/main/java/com/example/club_management/club_management_service/Repo/ClubManagementRepo.java
+++ Backend/Services/club-management-service/src/main/java/com/example/club_management/club_management_service/Repo/ClubManagementRepo.java
@@ -4,14 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.club_management.club_management_service.Model.ClubModel;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ClubManagementRepo extends JpaRepository<ClubModel, String> {
-    Optional<ClubModel> findByName(String name);
-    ClubModel findClubByClubId(UUID id);
-    ClubModel findClubByUserId(UUID userId);
+    Optional<ClubModel> findByClubName(String name);
+    Optional<ClubModel> findClubById(UUID id);
+    List<ClubModel> findClubByUserId(UUID userId);
 
-    void deleteByClubIdAndUserId(UUID clubId, UUID userId);
+
+    void deleteByIdAndUserId(UUID clubId, UUID userId);
 }
