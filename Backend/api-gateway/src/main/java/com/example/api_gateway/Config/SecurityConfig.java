2fd72eef5917d07b3f2689b62c3c72aeb469da93
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-22 05:16:01 +0530

    create file for api gateway config

---
+++ Backend/api-gateway/src/main/java/com/example/api_gateway/Config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.example.api_gateway.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
+public class SecurityConfig {
+
+
+}
