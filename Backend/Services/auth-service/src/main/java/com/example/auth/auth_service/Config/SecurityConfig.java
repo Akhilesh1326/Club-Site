0abe8f2248716676aee25307422531bf563c94f7
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-21 22:11:40 +0530

    update config for discovery of server realted routes

--- Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/Config/SecurityConfig.java
+++ Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/Config/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.example.auth.auth_service.Config;
 
 import org.springframework.context.annotation.Bean;
-import com.example.auth.auth_service.DTO.AuthUserDTO;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,13 +13,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    // Security configuration using SecurityFilterChain
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable() // Disable CSRF for stateless authentication like JWT
                 .authorizeRequests()
-                .requestMatchers("/api/auth-service/register", "/api/auth-service/greet", "/api/auth-service/auth-user/id/**", "/api/auth-service/auth-user/name/**").permitAll() // Public endpoints (updated method)
+                .requestMatchers("/api/auth-service/register", "/api/auth-service/greet", "/api/auth-service/auth-user/id/**", "/api/auth-service/auth-user/name/**").permitAll() // Public endpoints
+                .requestMatchers("/actuator/**").permitAll()  // Allow Eureka and monitoring
                 .requestMatchers(HttpMethod.GET).permitAll()
                 .anyRequest().authenticated() // Secure other endpoints
                 .and()
@@ -29,11 +27,8 @@ public class SecurityConfig {
         return http.build();
     }
 
-    // Password Encoder Bean
     @Bean
     public PasswordEncoder passwordEncoder() {
-
         return new BCryptPasswordEncoder(); // Using BCrypt for password hashing
     }
-
 }
