26b3a519cff273f921628e1a9c3f633b7ca1cd3b
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-22 05:10:59 +0530

    add securityFilterChain, authecitactionProvider, passwordEncoder and authenticationManager

--- Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/Config/SecurityConfig.java
+++ Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/Config/SecurityConfig.java
@@ -1,34 +1,73 @@
 package com.example.auth.auth_service.Config;
 
+import com.example.auth.auth_service.Filter.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() // Disable CSRF for stateless authentication like JWT
-                .authorizeRequests()
-                .requestMatchers("/api/auth-service/register", "/api/auth-service/greet", "/api/auth-service/auth-user/id/**", "/api/auth-service/auth-user/name/**").permitAll() // Public endpoints
-                .requestMatchers("/actuator/**").permitAll()  // Allow Eureka and monitoring
-                .requestMatchers(HttpMethod.GET).permitAll()
-                .anyRequest().authenticated() // Secure other endpoints
-                .and()
-                .cors(); // Enable CORS globally
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for stateless authentication like JWT
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth-service/register", "/api/auth-service/greet", "/api/auth-service/auth-user/id/**", "/api/auth-service/login").permitAll() // Public endpoints
+                        .requestMatchers("/actuator/**").permitAll()  // Allow Eureka and monitoring
+                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .anyRequest().authenticated() // Secure other endpoints.
+
+                )
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .cors(cors -> {}); // Enable CORS globally
         return http.build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // Using BCrypt for password hashing
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        provider.setUserDetailsService(userDetailsService);
+
+
+        return provider;
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12); // Using BCrypt for password hashing
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+
+    }
+
 }
