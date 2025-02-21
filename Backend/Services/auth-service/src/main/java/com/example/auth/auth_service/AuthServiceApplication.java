997772e694c5b64b100eefb062c3b484baeb9375
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-21 22:11:12 +0530

    add anotation for the enabling the eureka server discvoery mech

--- Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/AuthServiceApplication.java
+++ Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/AuthServiceApplication.java
@@ -1,9 +1,11 @@
 package com.example.auth.auth_service;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
