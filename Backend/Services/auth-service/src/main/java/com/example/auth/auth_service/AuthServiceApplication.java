e25a72e920c8e04d16aeddea79e91d9b4544fbf6
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-22 05:09:48 +0530

    modify for testing purpose, take back it to inital stage

--- Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/AuthServiceApplication.java
+++ Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/AuthServiceApplication.java
@@ -3,7 +3,6 @@ package com.example.auth.auth_service;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AuthServiceApplication {
