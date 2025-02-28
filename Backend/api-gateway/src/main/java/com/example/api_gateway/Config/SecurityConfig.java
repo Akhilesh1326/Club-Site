a2d11688c6797eb671f12c1fed7162f0b3496309
Akhilesh26 <akhileshpimple3@gmail.com>
2025-03-01 03:36:45 +0530

    add sec config for jwt

--- Backend/api-gateway/src/main/java/com/example/api_gateway/Config/SecurityConfig.java
+++ Backend/api-gateway/src/main/java/com/example/api_gateway/Config/SecurityConfig.java
@@ -1,10 +1,18 @@
 package com.example.api_gateway.Config;
 
+import com.example.api_gateway.Filter.JWTAuthFilter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.server.WebFilter;
 
+@Configuration
 public class SecurityConfig {
 
-
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public WebFilter jwtAuthFilter(JWTAuthFilter jwtAuthFilter) {
+        return jwtAuthFilter;
+    }
 }
