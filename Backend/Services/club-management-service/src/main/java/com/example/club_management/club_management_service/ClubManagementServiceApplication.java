ee3ac9dc03df142d5d5c9dd64780c4181f4c3e25
Akhilesh26 <akhileshpimple3@gmail.com>
2025-03-13 23:58:20 +0530

    update new info

--- Backend/Services/club-management-service/src/main/java/com/example/club_management/club_management_service/ClubManagementServiceApplication.java
+++ Backend/Services/club-management-service/src/main/java/com/example/club_management/club_management_service/ClubManagementServiceApplication.java
@@ -3,10 +3,11 @@ package com.example.club_management.club_management_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableDiscoveryClient
 public class ClubManagementServiceApplication {
 
 	public static void main(String[] args) {
