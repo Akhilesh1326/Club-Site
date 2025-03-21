08b91513b4fc9305295bfc286ceb61c9ad4e0694
Akhilesh26 <akhileshpimple3@gmail.com>
2025-03-21 23:24:15 +0530

    minor changes

--- Backend/Services/user-management-service/src/main/java/com/example/user_management/user_management_service/UserManagementServiceApplication.java
+++ Backend/Services/user-management-service/src/main/java/com/example/user_management/user_management_service/UserManagementServiceApplication.java
@@ -2,10 +2,9 @@ package com.example.user_management.user_management_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
-@EnableFeignClients
+@EnableDiscoveryClient
 public class UserManagementServiceApplication {
 
 	public static void main(String[] args) {
