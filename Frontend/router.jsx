38e4031681f8b8dc7b6732e9ef3f14aa3521b746
Akhilesh26 <akhileshpimple3@gmail.com>
2025-03-25 18:36:35 +0530

    change router for registration and change typo of router

--- Frontend/router.jsx
+++ Frontend/router.jsx
@@ -1,26 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import LandingPage from "./src/pages/LandingPage.jsx";
-import SignUp from './src/features/auth/SignUp.jsx';
-import SingUpRole from './src/features/auth/SingUpRole.jsx';
-import SignIn from "./src/features/auth/SignIn.jsx";
+import Register from "./src/features/auth/RegistrationForm.jsx";
+import Login from "./src/features/auth/LogIn.jsx"
 import HomePage from "./src/pages/HomePage.jsx";
 
 import ClubsPage from "./src/pages/ClubsPage.jsx";
 import ExplorePage from "./src/pages/ExplorePage.jsx";
 import InternalCommunityPage from "./src/pages/InternalCommunityPage.jsx";
-// import ClubLeaderDashboard from "./src/features/dashboard/ClubLeaderDashboard.jsx";
+import ClubLeaderDashboard from "./src/features/dashboard/ClubLeaderDashboard.jsx";
+
+import AdminProfile from "./src/components/profile/AdminProfile.jsx";
+import ModeratorProfile from "./src/components/profile/ModeratorProfile.jsx";
 
 const router = createBrowserRouter([
     { path: "/", element: <LandingPage /> },
-    { path: "/sign-up", element: <SignUp/>},
-    { path: "/sign-up-role", element: <SingUpRole/>},
-    { path: "/sign-in", element: <SignIn/>},
+    { path: "/register", element: <Register/>},
+    { path: "/log-in", element: <Login/>},
     { path: "/home", element: <HomePage/>},
     { path: "/explore", element: <ExplorePage/>},
     { path: "/community", element: <InternalCommunityPage/>},
     { path: "/clubs", element: <ClubsPage/>},
-    // { path: "/club-leader-dashboard", element: <ClubLeaderDashboard/>},
+    { path: "/admin", element: <AdminProfile/>},
+    { path: "/mod", element: <ModeratorProfile/>},
+    { path: "/club-leader-dashboard", element: <ClubLeaderDashboard/>}, 
   ])
 
 export default router;
