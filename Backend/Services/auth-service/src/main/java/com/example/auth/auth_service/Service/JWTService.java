18d9829d055f594848231862286b517ad68535f8
Akhilesh26 <akhileshpimple3@gmail.com>
2025-03-27 20:23:22 +0530

    add role and userid to jwt token

--- Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/Service/JWTService.java
+++ Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/Service/JWTService.java
@@ -1,5 +1,6 @@
 package com.example.auth.auth_service.Service;
 
+import com.example.auth.auth_service.Model.Auth_Users;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
@@ -12,6 +13,7 @@ import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import java.util.function.Function;
 
 
@@ -20,14 +22,16 @@ public class JWTService {
 
     private String secretKey = "KWYhl/YNG4smxQfnh+D/868qiQV4bHgKzQSezvS3gNQ=";
 
-    public String generateToken(String username) {
+    public String generateToken(String username, UUID userAuthId, Auth_Users.Role role) {
         Map<String, Object> claims = new HashMap<>();
+        claims.put("userId", userAuthId);
+        claims.put("role", role.toString());
       return   Jwts.builder()
                 .claims()
                 .add(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis()+60*60*30))
+                .expiration(new Date(System.currentTimeMillis()+60*60*1000))
                 .and()
                 .signWith(getKey())
                 .compact();
