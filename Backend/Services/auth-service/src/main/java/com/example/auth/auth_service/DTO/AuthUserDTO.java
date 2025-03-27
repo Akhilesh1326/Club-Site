2c4d2a9613586eed96733449aee4d52766e2cde5
Akhilesh26 <akhileshpimple3@gmail.com>
2025-03-27 20:19:26 +0530

    merger userDTO to authDto from user management service

--- Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/DTO/AuthUserDTO.java
+++ Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/DTO/AuthUserDTO.java
@@ -1,10 +1,56 @@
 package com.example.auth.auth_service.DTO;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
+
 public class AuthUserDTO {
 
     private String userName;
     private String email;
     private String password;
+    private String firstName;
+    private String lastName;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate DOB;
+
+    private String collegeOrUniversityName;
+    private String role;
+    private String phoneNumber;
+    private String profile_picture_url;
+
+    public void setProfile_picture_url(String profile_picture_url){
+        this.profile_picture_url = profile_picture_url;
+    }
+
+    public String getProfile_picture_url(){
+        return profile_picture_url;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+
+    public LocalDate getDOB(){
+        return this.DOB;
+    }
+
+    public String getCollegeOrUniversityName(){
+        return this.collegeOrUniversityName;
+    }
+
 
 
     public String getUserName(){
@@ -36,6 +82,12 @@ public class AuthUserDTO {
                 "email='" + email + '\'' +
                 ", password='" + password + '\'' +
                 ", userName='" + userName + '\'' +
+                ", first name ='" + firstName + '\'' +
+                ", last name ='" + lastName + '\'' +
+                ", role ='" + role + '\'' +
+                ", phone  ='" + phoneNumber + '\'' +
+                ", dob ='" + DOB + '\'' +
+                ", scool ='" + collegeOrUniversityName + '\'' +
                 '}';
     }
 }
