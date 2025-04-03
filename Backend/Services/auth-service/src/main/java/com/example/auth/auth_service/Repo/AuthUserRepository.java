ecddc9dd1a2078aeafd77a5997203d927c0b8878
Akhilesh26 <akhileshpimple3@gmail.com>
2025-04-03 22:50:04 +0530

    add custome query using @Query to fetch specific details

--- Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/Repo/AuthUserRepository.java
+++ Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/Repo/AuthUserRepository.java
@@ -1,10 +1,14 @@
 package com.example.auth.auth_service.Repo;
 
 
+import com.example.auth.auth_service.DTO.UserInfoDTO;
 import com.example.auth.auth_service.Model.Auth_Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,4 +19,12 @@ public interface AuthUserRepository extends JpaRepository<Auth_Users, UUID>{
     Auth_Users getByUserName(String userName);
     Optional<Auth_Users> findUserById(UUID id);
     Optional<Auth_Users> findUsersById(UUID id);
+
+    @Query("SELECT new com.example.auth.auth_service.DTO.UserInfoDTO(" +
+            "u.id, u.firstName, u.lastName, u.role, u.profilePictureUrl) " +
+            "FROM Auth_Users u WHERE u.id IN :memberIds")
+    List<UserInfoDTO> findUserDetailsByIds(@Param("memberIds") List<UUID> memberIds);
+
+
+
 }
