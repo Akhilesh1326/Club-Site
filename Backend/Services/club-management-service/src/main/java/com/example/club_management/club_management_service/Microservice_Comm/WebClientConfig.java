a65031dbbc3b445ffdfaeb89d7e443f229d5b1a3
Akhilesh26 <akhileshpimple3@gmail.com>
2025-04-03 22:38:51 +0530

    removied @LoadBalancer to direct api call

--- Backend/Services/club-management-service/src/main/java/com/example/club_management/club_management_service/Microservice_Comm/WebClientConfig.java
+++ Backend/Services/club-management-service/src/main/java/com/example/club_management/club_management_service/Microservice_Comm/WebClientConfig.java
@@ -1,6 +1,5 @@
 package com.example.club_management.club_management_service.Microservice_Comm;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,7 +7,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    @LoadBalanced
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
