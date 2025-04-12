9a16c0a42cad00b315c104073f21f12e5e7776c0
Akhilesh26 <akhileshpimple3@gmail.com>
2025-04-12 05:59:26 +0530

    create route for create event and get all events

--- Backend/Services/event-management-service/src/main/java/com/example/event_management/event_management_service/Controller/EventManagementController.java
+++ Backend/Services/event-management-service/src/main/java/com/example/event_management/event_management_service/Controller/EventManagementController.java
@@ -9,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @CrossOrigin
 @RestController
@@ -17,26 +20,68 @@ import java.util.List;
 public class EventManagementController {
     private final EventManagementService eventManagementService;
     @Autowired
-    private EventManagementModel eventManagementModel;
 
-    public EventManagementController(EventManagementService eventManagementService){
+
+    public EventManagementController(EventManagementService eventManagementService ){
         this.eventManagementService = eventManagementService;
+
     }
 
     @PostMapping("/create-event")
-    public ResponseEntity<?> createEvent(@RequestBody EventManagementDTO eventManagementDTO){
-        return ResponseEntity.ok(eventManagementService.createEvent(eventManagementDTO));
+    public Map<String, Object> createEvent(@RequestHeader("userId") String userId,  @RequestBody EventManagementDTO eventManagementDTO){
+        System.out.println("event name = "+eventManagementDTO);
+        Map<String, Object> res = new HashMap<>();
+        if(eventManagementDTO.getTitle() == null || eventManagementDTO.getTitle().trim().isEmpty()){
+            res.put("Status", "No Title of event is given");
+            return res;
+        }
+        if(eventManagementDTO.getCategory() == null || eventManagementDTO.getCategory().trim().isEmpty()){
+            res.put("Status", "No category is given to event");
+            return res;
+        }
+        if(eventManagementDTO.getDescription() == null || eventManagementDTO.getDescription().trim().isEmpty()){
+            res.put("Status", "No description is given to event");
+            return res;
+        }
+        if(eventManagementDTO.getLocation() == null || eventManagementDTO.getLocation().trim().isEmpty()){
+            res.put("Status", "No Location of event is given");
+            return res;
+        }
+        if(eventManagementDTO.getStartTime() == null){
+            res.put("Status", "No start time is given");
+            return res;
+        }
+        if(eventManagementDTO.getEndTime() == null){
+            res.put("Status", "No end time is given");
+            return res;
+        }
+        if(eventManagementDTO.getMaxParticipants() == null){
+            res.put("Status", "No event max participant list is given");
+            return res;
+        }
+        if(eventManagementDTO.getRegistrationRequired() == null){
+            res.put("Status", "Registration is Information is required");
+            return res;
+        }
+        if(eventManagementDTO.getEventType() == null){
+            res.put("Status", "Event type must be given, College_Level or Global_Level");
+            return res;
+        }
+        EventManagementModel newEvent = eventManagementService.createEvent(UUID.fromString(userId), eventManagementDTO);
+        res.put("Status", "Success");
+        return res;
     }
 
-    @GetMapping("event")
+    @GetMapping("/events")
     public ResponseEntity<List<EventManagementModel>> getAllEvents(){
         List<EventManagementModel> events = eventManagementService.getAllEvents();
+        return ResponseEntity.ok(events);
     }
 
 
-    @GetMapping("/get-event-by-category/{category}")
-    public List<EventManagementModel> getEventByCategory(@RequestParam String category){
-        return ResponseEntity.ok(eventManagementService.getEventByCategory(category));
-    }
+//    @GetMapping("/get-event-by-category/{category}")
+//    public List<EventManagementModel> getEventByCategory(@RequestParam String category){
+//        return ResponseEntity.ok(eventManagementService.getEventByCategory(category));
+//    }
 
 }
