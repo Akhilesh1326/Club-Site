8431fa40057c4d879b999e9ee26cf6477ebfd7c6
Akhilesh26 <akhileshpimple3@gmail.com>
2025-04-12 06:40:37 +0530

    controller for participants

---
+++ Backend/Services/event-management-service/src/main/java/com/example/event_management/event_management_service/Controller/EventParticipantsController.java
@@ -0,0 +1,15 @@
+package com.example.event_management.event_management_service.Controller;
+
+
+import com.example.event_management.event_management_service.Service.EventParticipantService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/eventParticipant")
+public class EventParticipantsController {
+    private EventParticipantService;
+
+}
