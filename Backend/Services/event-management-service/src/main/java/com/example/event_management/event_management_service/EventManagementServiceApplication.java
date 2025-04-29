ba9205c30db4f28820b8a8b7597b30b49419d2e6
Akhilesh26 <akhileshpimple3@gmail.com>
2025-04-30 02:22:45 +0530

    add eurkea annotation for service discovery

--- Backend/Services/event-management-service/src/main/java/com/example/event_management/event_management_service/EventManagementServiceApplication.java
+++ Backend/Services/event-management-service/src/main/java/com/example/event_management/event_management_service/EventManagementServiceApplication.java
@@ -3,7 +3,10 @@ package com.example.event_management.event_management_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EventManagementServiceApplication {
 
 	public static void main(String[] args) {
