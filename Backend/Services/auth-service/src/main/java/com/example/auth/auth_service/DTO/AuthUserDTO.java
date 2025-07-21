860c25a4fba42f9e8eff5bc1fa628285e420418a
Akhilesh26 <akhileshpimple3@gmail.com>
2025-07-21 15:08:53 +0530

    update in DTO

--- Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/DTO/AuthUserDTO.java
+++ Backend/Services/auth-service/src/main/java/com/example/auth/auth_service/DTO/AuthUserDTO.java
@@ -1,7 +1,6 @@
 package com.example.auth.auth_service.DTO;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-
 import java.time.LocalDate;
 
 public class AuthUserDTO {
@@ -11,18 +10,66 @@ public class AuthUserDTO {
     private String password;
     private String firstName;
     private String lastName;
+
     @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate DOB;
 
     private String collegeOrUniversityName;
     private String role;
     private String phoneNumber;
-    private String profile_picture_url;
+    private String profilePictureUrl;  // Changed from profile_picture_url to follow camelCase
     private String state;
     private String city;
     private String department;
     private Integer year;
 
+    // Constructors
+    public AuthUserDTO() {}
+
+    // Getters
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public LocalDate getDOB() {
+        return this.DOB;
+    }
+
+    public String getCollegeOrUniversityName() {
+        return this.collegeOrUniversityName;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public String getProfilePictureUrl() {
+        return profilePictureUrl;
+    }
+
+    public String getState() {
+        return state;
+    }
 
     public String getCity() {
         return city;
@@ -32,87 +79,88 @@ public class AuthUserDTO {
         return department;
     }
 
-    public String getState() {
-        return state;
-    }
-
     public Integer getYear() {
         return year;
     }
 
-    public void setProfile_picture_url(String profile_picture_url){
-        this.profile_picture_url = profile_picture_url;
-    }
-
-    public String getProfile_picture_url(){
-        return profile_picture_url;
-    }
-
-    public String getRole() {
-        return this.role;
-    }
-
-    public String getFirstName(){
-        return this.firstName;
-    }
-
-    public String getLastName(){
-        return this.lastName;
-    }
-
-    public String getPhoneNumber(){
-        return this.phoneNumber;
-    }
-
-    public LocalDate getDOB(){
-        return this.DOB;
-    }
-
-    public String getCollegeOrUniversityName(){
-        return this.collegeOrUniversityName;
-    }
-
-
-
-    public String getUserName(){
-        return this.userName;
-    }
-    public void setUserName(String userName){
+    // Setters (THESE WERE MISSING!)
+    public void setUserName(String userName) {
         this.userName = userName;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDOB(LocalDate DOB) {
+        this.DOB = DOB;
+    }
+
+    public void setCollegeOrUniversityName(String collegeOrUniversityName) {
+        this.collegeOrUniversityName = collegeOrUniversityName;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setProfilePictureUrl(String profilePictureUrl) {
+        this.profilePictureUrl = profilePictureUrl;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
     }
 
     @Override
     public String toString() {
         return "AuthUserDTO{" +
-                "email='" + email + '\'' +
+                "userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
-                ", userName='" + userName + '\'' +
-                ", first name ='" + firstName + '\'' +
-                ", last name ='" + lastName + '\'' +
-                ", role ='" + role + '\'' +
-                ", phone  ='" + phoneNumber + '\'' +
-                ", dob ='" + DOB + '\'' +
-                ", state ='" + state + '\'' +
-                ", city ='" + city + '\'' +
-                ", dept ='" + department + '\'' +
-                ", yyear ='" + year + '\'' +
-                ", scool ='" + collegeOrUniversityName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", DOB=" + DOB +
+                ", collegeOrUniversityName='" + collegeOrUniversityName + '\'' +
+                ", role='" + role + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", profilePictureUrl='" + profilePictureUrl + '\'' +
+                ", state='" + state + '\'' +
+                ", city='" + city + '\'' +
+                ", department='" + department + '\'' +
+                ", year=" + year +
                 '}';
+    }
+
+    public String getProfile_picture_url() {
+        return profilePictureUrl;
     }
 }
